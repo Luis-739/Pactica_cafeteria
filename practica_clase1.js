af40d523f5a1bb11bb6b87f0649a32dec6d65f4a
Luis-739 <luisalbertoml6546@gmail.com>
2025-07-08 21:34:34 -0600

    first commit

---
+++ practica_clase1.js
@@ -0,0 +1,25 @@
+
+const listaOrdenes = document.getElementById('listaOrdenes');
+
+const addOrdenBtn = document.getElementById('botonAgregarPedido');
+
+let ordenid = 1;
+
+addOrdenBtn.addEventListener('click', () => {
+    const orden = {id: ordenid++, status: 'En proceso'};
+    botonAgregarPedido(orden);
+    processOrden(orden);
+})
+
+function botonAgregarPedido(orden) {
+    const listaItem = document.createElement('li');
+    orden.id = `Lista de pedidos-${orden.id}`;
+    listaItem.textContent = `Pedido #${orden.id}: ${orden.status}`;
+    listaOrdenes.appendchild(listaItem);
+};
+
+
+
+
+
+
